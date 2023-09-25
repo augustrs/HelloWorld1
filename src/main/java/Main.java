cc910f239b354d808ec18beef3b6fd32f54ed072
MarcusKM95 <mamo0005@stud.kea.dk>
2023-09-25 13:12:42 +0200

    Commit 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!!!!");
         System.out.println("Jeg synes nu Søren er ret sej");
+        System.out.println("Ekstra commit");
     }
 
 }
